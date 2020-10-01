9bf822828405384be79f5475b9aa28d72a2a8535
Григорий Бойко <boiko.gri@gmail.com>
2020-10-01 16:33:16 +0700

    Initial Commit

---
+++ CarDirectory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarDirectory
+//
+//  Created by Григорий Бойко on 01.10.2020.
+//  Copyright © 2020 Grigory Boyko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
